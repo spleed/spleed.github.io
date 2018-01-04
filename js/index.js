0a2112c8608677201f8985c92638baf042612020
Daniele Paviani <daniele@paviani.it>
2018-01-04 15:56:31 +0100

    pushState

--- js/index.js
+++ js/index.js
@@ -29,7 +29,7 @@ S.utils = {
         var uri = window.location.toString();
         if (uri.indexOf("?") > 0) {
             var clean_uri = uri.substring(0, uri.indexOf("?"));
-            window.history.replaceState({}, document.title, clean_uri);
+            window.history.pushState({}, document.title, clean_uri);
         }
     }
 }
